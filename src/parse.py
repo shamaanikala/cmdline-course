1fcb90cba4d373313ec0b4be82832ae9bdb528cf
V. Katajisto <18308619+shamaanikala@users.noreply.github.com>
2023-10-22 18:25:18 +0300

    Fixed parse.py

--- src/parse.py
+++ src/parse.py
@@ -1,8 +1,7 @@
-from sys import argv, stderr, settrace
+from sys import argv, stderr
 from bllipparser import RerankingParser
 
 
-settrace
 if __name__=="__main__":
     """
         Parse an English Gutenberg book in sentence per line format
@@ -16,20 +15,15 @@ if __name__=="__main__":
     # Initialize parser and download parser model if needed.
     rrp = RerankingParser.fetch_and_load('WSJ-PTB3', verbose=True)
 
-    print('Opening input file...')
     ifile = open(argv[1])
-    print('Opening output file...')
     ofile = open(argv[2],"w")
     
-    print('Starting parsing...')
     # Read input sentences from ifile and print parse trees to ofile.
     parsedLines = 0
     for i, line in enumerate(ifile):
-        print('> Parsing line',line)
         if len(line.strip('\n')) == 0:
             continue
         if parsedLines >= 10:
             break
-        #print(rrp.simple_parse(line.strip('\n')), file=ofile)
-        print(rrp.simple_parse(line.strip('\n')))
+        print(rrp.simple_parse(line.strip('\n')), file=ofile)
         parsedLines += 1
